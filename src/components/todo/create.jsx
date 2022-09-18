dfd4c33148fafaef1c74a5c6feae282346c3d7dd
EL-MEHDI LOUKACH <you@example.com>
2022-09-18 17:04:44 +0100

    [ADD] - UI creation

--- src/components/todo/create.jsx
+++ src/components/todo/create.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
-
-
+import {Col, Form, Button} from 'react-bootstrap'
+import StyleCreate from './create.module.css'
 export default function CreateToDo() {
     return (
         <>
-            <h1>Create</h1>
+            <h1 className={StyleCreate.heading}>Create New Task</h1>
+            <Col xl={12} lg={12} md={12} sm={12} xs={12} xxs={12} className={StyleCreate.container}>
+                <Form className={StyleCreate.form}>
+                    <Form.Group className={StyleCreate.formGroup}>
+                        <Form.Label className={StyleCreate.formLabel}>Task title : </Form.Label>
+                        <Form.Control className={StyleCreate.formControl} type="text" placeholder='Enter the title of the task' />
+                    </Form.Group>
+                    <Form.Group className={StyleCreate.formGroup}>
+                        <Form.Label className={StyleCreate.formLabel}>Task Description : </Form.Label>
+                        <Form.Control className={StyleCreate.formControl} as="textarea" rows="5" placeholder='Enter the description of the task' />
+                    </Form.Group>
+                    <Form.Group className={StyleCreate.formGroup}>
+                        <Button className={StyleCreate.submitBtn}>Create</Button>
+                    </Form.Group>
+                </Form>
+            </Col>
         </>
     )
 }
--- src/components/todo/list.jsx
+++ src/components/todo/list.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
-
+import {Col, Table} from 'react-bootstrap'
+import StyleList from './list.module.css'
 
 export default function ListOfToDos() {
     return (
         <>
-        <h1>List</h1>
+        <h1 className={StyleList.heading}>List of all to do lists</h1>
+            <Col xl={12} lg={12} md={12} sm={12} xs={12} xxs={12} className={StyleList.container}>
+                <Table striped bordered hover>
+                    <thead>
+                        <tr>
+                        <th>#</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Username</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                        <td>1</td>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                        </tr>
+                    </tbody>
+                </Table>
+            </Col>
         </>
     )
 }
