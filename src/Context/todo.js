897b01c8025284df93cebe145e303f03aabefb11
El-Mehdi Loukach <42277331+EL-MehdiLoukach@users.noreply.github.com>
2022-09-19 12:07:57 +0100

    Update todo.js

--- src/Context/todo.js
+++ src/Context/todo.js
@@ -1,18 +1,28 @@
 import React, {createContext, useState} from 'react'
 // import MainLayout from '../components/layouts/main'
-
+import axios from 'axios'
 
 
 
 export const TodoContext = createContext();
 
 export const TodoContextProvider = ({children}) => {
-    const [todo, setTodo] = useState()
-    const postTodo = (title, description) => {
-        setTodo(todo, {
-            title: title,
-            description: description
+    const [todo, setTodo] = useState([])
+    const postTodo = (url,title, description) => {
+        axios.post(url, {
+            title : title
+            description : description
         })
+        .then((res)=>{
+            setTodo([...todo], {
+                title: title,
+                description: description
+            })
+        })
+        .catch((err) => {
+        
+        })
+        
         console.log(todo)
     }
     return (
