ce6206914c1b45584820fb67022edd80c4b4e8b9
abduu am <abdelouahed863@gmail.com>
2022-09-19 18:57:02 +0100

    list.jsx modified

--- src/components/todo/list.jsx
+++ src/components/todo/list.jsx
@@ -4,39 +4,41 @@ import { TaskContext } from "../context/todoContext";
 import StyleList from "./list.module.css";
 
 export default function ListOfToDos() {
-  const { tasks } = useContext(TaskContext);
-
-  return (
-    <>
-      <h1 className={StyleList.heading}>List of all to do lists</h1>
-      <Col
-        xl={12}
-        lg={12}
-        md={12}
-        sm={12}
-        xs={12}
-        xxs={12}
-        className={StyleList.container}
-      >
-        <Table striped bordered hover>
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>First Name</th>
-              <th>Last Name</th>
-              <th>Username</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>1</td>
-              <td>{tasks.title}</td>
-              <td>{tasks.description}</td>
-              <td>@whatever</td>
-            </tr>
-          </tbody>
-        </Table>
-      </Col>
-    </>
-  );
+  const { task } = useContext(TaskContext);
+  console.log(task);
+  task?.map((e, i) => {
+    return (
+      <>
+        <h1 className={StyleList.heading}>List of all to do lists</h1>
+        <Col
+          xl={12}
+          lg={12}
+          md={12}
+          sm={12}
+          xs={12}
+          xxs={12}
+          className={StyleList.container}
+        >
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Username</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr key={i}>
+                <td>1</td>
+                <td>{e.title}</td>
+                <td>{e.description}</td>
+                <td>@whatever</td>
+              </tr>
+            </tbody>
+          </Table>
+        </Col>
+      </>
+    );
+  });
 }
