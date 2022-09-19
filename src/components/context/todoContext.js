f7a24b65fcbb64f35d87be151d7ca995f59f31e5
El-Mehdi Loukach <42277331+EL-MehdiLoukach@users.noreply.github.com>
2022-09-19 12:02:12 +0100

    Update todoContext.js

--- src/components/context/todoContext.js
+++ src/components/context/todoContext.js
@@ -1,13 +1,25 @@
 import React, { createContext, useState } from "react";
-
+import axios from 'axios'
 export const TaskContextProvider = ({ children }) => {
   const [tasks, setTasks] = useState([]);
-  const createTask = (title, description, id) => {
-    setTasks = {
-      id: id,
-      title: title,
-      description: description,
-    };
+  const [sucess, setSuccess] = useState()
+  const [loading, setloading] = useState(false)
+  const createTask = (url,task) => {
+    setLoading(true)
+    axios.post(url,task)
+    .then((res) => {
+     setTasks([...tasks, {
+        title: task.title,
+        description: task.description,
+      }])
+      setSuccess(res.message)
+      setLoading(false)
+    })
+    .catch((err)=>{
+      console.log(err)
+      setLoading(false)
+    })
+    
   };
   return (
     <TaskContext.Provider value={{ tasks, createTask }}>
